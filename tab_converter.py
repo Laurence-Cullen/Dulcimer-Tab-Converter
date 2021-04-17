6badec580ced83c435ca00c372534972b114da04
Laurence Cullen <laurencesimoncullen@gmail.com>
2021-04-17 19:38:04 +0100

    readding notes into tab_converter.py ad cloud function requires a single file

--- tab_converter.py
+++ tab_converter.py
@@ -1,10 +1,106 @@
-import midi
+import pathlib
+
 import flask
-from notes import bottom_string, all_notes, note_to_dulcimer_string, midi_pitch_to_note
+import midi
+
+bottom_string = 'E'
+
+octave0 = ["Eb"]
+octave1 = ["E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#"]
+octave2 = ["e", "f", "f#", "g", "g#", "a", "a#", "b", "c", "c#", "d", "d#"]
+octave3 = ["e'", "f'", "f#'", "g'", "g#'", "a'", "a#'", "b'", "c'", "c#'", "d'", "d#'"]
+octave4 = ["e''", "f''", "f#''", "g''", "g#''", "a''", "a#''", "b''", "c''", "c#''", "d''", "d#''"]
+
+all_notes = octave0 + octave1 + octave2 + octave3 + octave4
+
+midi_pitch_to_note = {
+    99: "d#''",
+    98: "d''",
+    97: "c#''",
+    96: "c''",
+    95: "b''",
+    94: "a#''",
+    93: "a''",
+    92: "g#''",
+    91: "g''",
+    90: "f#''",
+    89: "f''",
+    88: "e''",
+    87: "d#'",
+    86: "d'",
+    85: "c#'",
+    84: "c'",
+    83: "b'",
+    82: "a#'",
+    81: "a'",
+    80: "g#'",
+    79: "g'",
+    78: "f#'",
+    77: "f'",
+    76: "e'",
+    75: "d#",
+    74: "d",
+    73: "c#",
+    72: "c",
+    71: "b",
+    70: "a#",
+    69: "a",
+    68: "g#",
+    67: "g",
+    66: "f#",
+    65: "f",
+    64: "e",
+    63: "D#",
+    62: "D",
+    61: "C#",
+    60: "C",
+    59: "B",
+    58: "A#",
+    57: "A",
+    56: "G#",
+    55: "G",
+    54: "F#",
+    53: "F",
+    52: "E",
+    51: "Eb"
+}
+
+# Ellie's standard octave, maybe octave up from standard guitar octave
+note_to_dulcimer_string = {
+    "E": "[1]",
+    "G": "[2]",
+    "A": "[3]",
+    "B": "[4]",
+    "C": "[5]",
+    "C#": "1",
+    "D": "[6]/2",
+    "D#": "3",
+    "e": "[7]",
+    "f": "[8]",
+    "f#": "4",
+    "g": "5",
+    "g#": "[9]/(1)",
+    "a": "[10]/6/(2)",
+    "a#": "[11]/(3)",
+    "b": "[12]/7",
+    "c": "8",
+    "c#": "(4)",
+    "d": "9/(5)",
+    "d#": "10",
+    "e'": "(6)",
+    "f'": "11",
+    "f#'": "(7)",
+    "g'": "(8)",
+    "g#'": "12",
+    "a'": "(9)",
+    "a#'": "(10)",
+    "c'": "(11)",
+    "d#'": "(12)",
+}
 
 
-def get_midi_pitches(file_name):
-    song = midi.read_midifile(file_name)
+def get_midi_pitches(file_path):
+    song = midi.read_midifile(file_path)
     song.make_ticks_abs()
 
     track = song[0]
@@ -237,7 +333,7 @@ def pitches_to_tab(pitches: dict, semitone_transpose: int):
         raise ValueError(f"got {max_simultaneous_notes} max concurrent notes but humans only have two hands!")
 
     top_tab = ""
-    bottom_tab = ""
+    # bottom_tab = ""
 
     unit_width = get_unit_width()
 
@@ -252,7 +348,9 @@ def pitches_to_tab(pitches: dict, semitone_transpose: int):
         if len(pitches_at_tick) == 2:
             continue
 
-        top_tab += note_to_dulc_tab_string(note_to_dulc_tab_string(midi_pitch_to_note[pitches_at_tick[0]]), unit_width)
+        top_tab += note_to_dulc_tab_string(
+            note_to_dulc_tab_string(midi_pitch_to_note[pitches_at_tick[0]], unit_width), unit_width
+        )
 
 
 options_headers = {
@@ -279,7 +377,10 @@ def guitar_to_dulcimer_tab(request: flask.request):
     else:
         return 'No guitar tab passed in', 200, main_headers
 
-    dulcimer_tab = guitar_tab_lines_to_dulcimer(parse_tab_string(tab_string), semitone_transpose=semitone_transpose)
+    dulcimer_tab = guitar_tab_lines_to_dulcimer(
+        tab_lines=parse_tab_string(tab_string),
+        semitone_transpose=semitone_transpose
+    )
 
     return dulcimer_tab, 200, main_headers
 
@@ -289,8 +390,9 @@ def midi_to_dulcimer_tab(request: flask.request):
     if request.method == 'OPTIONS':
         return '', 204, options_headers
 
-    f = request.files['midi_file']
-    f.save(f.filename)
+    midi_file = request.files['midi_file']
+    midi_save_path = pathlib.Path(f"/tmp/{midi_file.filename}")
+    midi_file.save(midi_save_path)
 
     request_json = request.get_json()
     if request_json and 'tab' in request_json:
@@ -298,7 +400,7 @@ def midi_to_dulcimer_tab(request: flask.request):
     else:
         return 'No guitar tab passed in', 200, main_headers
 
-    pitches = get_midi_pitches(f.filename)
+    pitches = get_midi_pitches(midi_save_path)
 
     tab = pitches_to_tab(pitches, semitone_transpose)
 
